2d58af927022db9e82f057ccb4a4e5d8b6e87c9c
myhauntedspoon <drewhinckley2009@gmail.com>
2024-07-24 10:09:44 -0500

    make buttons work

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   const container = document.querySelector(".button-container");
 
-  const buttonTexts = [
-    "What is chicken?",
-    "blank",
-    "blank",
-    "blank",
-    "blank",
-    "Where can I find chicken?",
+  const buttonText = [
+    "what-is-chicken.html",
+    "blank.html",
+    "blank.html",
+    "blank.html",
+    "blank.html",
+    "where-can-i-find-chicken.html",
   ];
-  for (let i = 0; i < buttonTexts.length; i++) {
+
+  const handleButtonClick = (index) => {
+    const fileName = buttonText[index - 1];
+    if (fileName !== "blank.html") {
+      window.location.href = `answers/${fileName}`;
+    }
+  };
+
+  for (let i = 0; i < buttonText.length; i++) {
     const button = document.createElement("button");
     button.id = `button${i + 1}`;
-    button.textContent = buttonTexts[i];
+    button.textContent = buttonText[i].replace(".html", "").replace(/-/g, " ");
     button.addEventListener("click", () => handleButtonClick(i + 1));
     container.appendChild(button);
   }
 });
-``;
